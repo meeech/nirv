9b315ea150f70c63e43030ff043853ca18eb3521
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-12-20 23:51:36 -0500

    basically taking boom gem, and turning it into my skel - copying it over piece by piece, figuring out what the parts are doing. its one way to learn :)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,20 @@
-require 'rubygems'
-require 'nirvanahq'
 require 'test/unit'
+
+begin
+  require 'rubygems'
+  require 'redgreen'
+rescue LoadError
+end
+
+require 'mocha'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'nirvanahq'
+
+def boom_json(name)
+  Boom::Storage.any_instance.stubs(:json_file).
+    returns("test/examples/#{name}.json")
+  Boom.stubs(:storage).returns(Boom::Storage.new)
+end
