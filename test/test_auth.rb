1cde9a3b794f39285cb77d3641e45740f557a218
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-12-21 01:43:28 -0500

    2 tests passing. tdd can be fun.

--- test/test_auth.rb
+++ test/test_auth.rb
@@ -3,18 +3,18 @@ require "helper"
 module NirvanaHQAuthTests
   
   class NirvanaHQAuthTest < Test::Unit::TestCase
-    
+
     def setup
       @nirvauth = NirvanaHQ::Auth.new $nirvana_config
     end
-    
-    def test_token_file_exists
-      assert File.exists?(NirvanaHQ::Auth::TOKEN_FILE)
-    end
+
+    # def test_token_file_exists
+    #   assert File.exists?(NirvanaHQ::Auth::TOKEN_FILE)
+    # end
     
     def test_fetch_token
-      token = '1231231231231312312312333123111111'
-      assert_equal 36, token.length
+      token = @nirvauth.fetch_token
+      assert_equal 32, token.length
     end
     
   end
