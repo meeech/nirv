f8b074bc3625c3cc7f39b78e4fd39bad7895d764
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-12-21 11:28:28 -0500

    Added bundler code to refresh the Gemfile.lock from the gemspec. This may not be in the right place?

--- test/helper.rb
+++ test/helper.rb
@@ -8,6 +8,17 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'nirvanahq'
 
+#Refresh the Gemfile.lock
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+
+
 def task_json
   File.open("examples/task.json", 'r').read
 end
