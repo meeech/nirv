73eec8788ea78b7a924c3baf5113a7e8e2a7f39b
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-12-22 13:43:25 -0500

    ok, solved the ordering problem - realized i can break things into test cases. get rid of all sleeps.

--- test/test_task.rb
+++ test/test_task.rb
@@ -1,11 +1,56 @@
 require File.dirname(__FILE__) + "/helper"
 
-
 module NirvanaHQTests
 
+  TASK_YAML = File.dirname(__FILE__) + "/examples/task.yaml"
+
+  class NirvanaHQAddTrashTaskTest < Test::Unit::TestCase
+
+    def setup
+      @task = YAML::load_file(TASK_YAML)
+      @nirvana = NirvanaHQ.new $nirvana_config
+    end
+
+    def new_task
+      task = {
+        "id" => UUID.generate,
+        "name" => "Test Task #{rand(100)}",
+        "_name" => Time.now.to_i
+      }
+      
+      File.open( TASK_YAML, 'w' ) do |out|
+         YAML.dump( task, out )
+      end
+      
+      task
+    end
+
+    def test_add_task      
+      task = self.new_task
+      puts "Creating #{task['name']}\n"
+      # task.add expects a hash
+      result = @nirvana.add task
+      result = JSON.parse(result)
+
+      assert result.keys.include?('results')
+      assert result['results'][0]
+      assert result['results'][0].keys.include?('task')
+      assert_equal task["id"], result['results'][0]['task']['id']
+    end
+
+    def test_trash_task
+      result = @nirvana.trash @task['id']
+      result = JSON.parse(result)
+
+      assert result.keys.include?('results')
+      assert result['results'][0]
+      assert result['results'][0].keys.include?('task')
+      assert_equal "6", result['results'][0]['task']['state']
+    end 
+
+  end
+
   class NirvanaHQTaskTest < Test::Unit::TestCase
-    
-    TASK_YAML = File.dirname(__FILE__) + "/examples/task.yaml"
     
     def setup
       @task = YAML::load_file(TASK_YAML)
@@ -25,52 +70,11 @@ module NirvanaHQTests
       assert keys.include? "task"
     end
 
-    def new_task
-      task = {
-        "id" => UUID.generate,
-        "name" => "Test Task #{rand(100)}",
-        "_name" => Time.now.to_i
-      }
-      
-      File.open( TASK_YAML, 'w' ) do |out|
-         YAML.dump( task, out )
-      end
-      
-      return task
-    end
-
-    def test_add_task      
-      task = self.new_task
-      puts "Creating #{task['name']}\n"
-      # task.add expects a hash
-      result = @nirvana.add task
-      result = JSON.parse(result)
-
-      assert result.keys.include?('results')
-      assert result['results'][0]
-      assert result['results'][0].keys.include?('task')
-      assert_equal task["id"], result['results'][0]['task']['id']
-    end
-
     # def test_get_task
     # 
     #   assert false
     # end
     
-    # this fails when run as part of all the tests, but is fine when run solo, after running add.
-    # most likely just a timeing issue or order of tests issue. 
-    # need depends on, which should be part of a testing lib somewhere.
-    # ie: once a test has been marked deleted, you cant trash it, so this fails. 
-    def test_trash_task
-      result = @nirvana.trash @task['id']
-      result = JSON.parse(result)
-
-      assert result.keys.include?('results')
-      assert result['results'][0]
-      assert result['results'][0].keys.include?('task')
-      assert_equal "6", result['results'][0]['task']['state']
-    end 
-
     # more destructive. No way to recover (afaik) via ui
     # here more as an example than anything else.
     def test_delete_task
@@ -93,7 +97,7 @@ module NirvanaHQTests
       assert_equal "0", result['results'][0]['task']['deleted']      
       
       # pause for a sec, to make sure delete timestamp is later. clean up
-      sleep 1
+
       @nirvana.delete @task['id']
     end
 
